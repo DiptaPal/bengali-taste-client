5ee6938428c7c615f8af02cbd725ea1fb067e2a6
DiptaPal <diptapal1998@gmail.com>
2022-11-08 09:45:52 +0600

    react-photo-view was done

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const Service = ({ service }) => {
     const { title, price, url, description, _id } = service
     return (
         <div>
-            <div className="p-6 rounded-md card card-compact bg-white shadow-md min-h-full">
-                <figure><img src={url} className='w-full h-[210px] object-cover rounded-lg' alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title text-3xl font-bold uppercase my-2">{title}</h2>
-                    <div className=''>
-                        <p className='text-2xl text-activeColor font-semibold mb-2'>Price: ${price}</p>
-                        <p className='text-justify'>
-                            {description.length > 100 ?
-                                `${description.split(0, 100)} . . .`
-                                :
-                                description
-                            }
-                        </p>
-                        <button className='w-full mt-4 py-3 font-medium bg-activeColor text-white transition duration-200 rounded-md shadow-md hover:bg-gray-300 hover:text-activeColor text-xl'>View Details</button>
+            <PhotoProvider>
+                <div className="p-6 rounded-md card card-compact bg-white shadow-md min-h-full">
+                    <figure>
+                    <PhotoView src={url}
+                    >
+                        <img src={url} className='w-full h-[210px] object-cover rounded-lg' alt="Shoes" />
+                    </PhotoView>
+                        </figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-3xl font-bold uppercase my-2">{title}</h2>
+                        <div className=''>
+                            <p className='text-2xl text-activeColor font-semibold mb-2'>Price: ${price}</p>
+                            <p className='text-justify'>
+                                {description.length > 100 ?
+                                    `${description.split(0, 100)} . . .`
+                                    :
+                                    description
+                                }
+                            </p>
+                            <button className='w-full mt-4 py-3 font-medium bg-activeColor text-white transition duration-200 rounded-md shadow-md hover:bg-gray-300 hover:text-activeColor text-xl'>View Details</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </PhotoProvider>
         </div>
     );
 };
